56c792c1cdde3524f7bbe8cbf12bf270dc48f430
Ayoub Chikhi <ayoub.chikhi@etu.u-paris.fr>
2025-10-20 09:58:54 +0200

    bug bouton faire un don + carousel dans un autre fichier js

--- assets/js/don.js
+++ assets/js/don.js
@@ -1,23 +1,7 @@
+
+
+
 const boutonOuvrirDon = document.getElementById('ouvrir-don');
-
-const slides = document.getElementById("slides");
-const totalSlides = document.querySelectorAll("#slides .slide").length;
-let currentIndex = 0;
-
-document.getElementById("next-mission").addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    updateCarousel();
-});
-
-document.getElementById("prev-mission").addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    updateCarousel();
-});
-
-function updateCarousel() {
-    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
 const fondAssombri = document.getElementById('fond-assombri');
 const modales = document.querySelectorAll('.modale');
 const boutonFermer = document.getElementById('fermer-tout');
