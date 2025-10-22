1e260e7d618a76a0483a933a21d4948b1fbe64f0
Ayoub Chikhi <ayoub.chikhi@etu.u-paris.fr>
2025-10-22 12:04:27 +0200

    php, include

---
+++ pages/footer.php
@@ -0,0 +1,47 @@
+<?php
+?>
+<footer class="flex flex-col md:flex-row w-full px-12 pt-20 pb-12 gap-8">
+    <div class="mr-1">
+        <p class="text-blue-500 font-bold text-4xl">EGEE</p>
+        <p>Entente des Générations pour l’Emploi et L’entreprise</p>
+        <p>Association reconnu d’utilité publique</p>
+
+        <div class="flex gap-4 items-center mt-8">
+            <a href="https://www.instagram.com/association_egee/" class="transform transition duration-300 hover:scale-125">
+                <img src="assets/img/instagram.svg" alt="">
+            </a>
+            <a href="https://fr.linkedin.com/company/egee-asso/" class="transform transition duration-300 hover:scale-125">
+                <img src="assets/img/linkedin.svg" alt="">
+            </a>
+            <a href="https://www.facebook.com/egee.asso/" class="transform transition duration-300 hover:scale-125">
+                <img src="assets/img/facebook.svg" alt="">
+            </a>
+        </div>
+    </div>
+
+    <div id="info_section_footer" class="flex flex-row gap-6 md:gap-16 md:ml-auto">
+        <div class="flex flex-col">
+            <p class="font-bold text-xl mb-4 uppercase">Nos actions</p>
+            <a href="pages/Nos_Missions.html" class="transition hover:text-blue-500">Education</a>
+            <a href="#" class="transition hover:text-blue-500">Emploi</a>
+            <a href="#" class="transition hover:text-blue-500">Entreprise</a>
+            <a href="#" class="transition hover:text-blue-500">Formation</a>
+        </div>
+
+        <div class="flex flex-col">
+            <p class="font-bold text-xl mb-4 uppercase">Nos missions</p>
+            <a href="#" class="transition hover:text-blue-500">Education</a>
+            <a href="#" class="transition hover:text-blue-500">Emploi</a>
+            <a href="#" class="transition hover:text-blue-500">Entreprise</a>
+            <a href="#" class="transition hover:text-blue-500">Formation</a>
+        </div>
+
+        <div class="flex flex-col">
+            <p class="font-bold text-xl mb-4 uppercase">à propos</p>
+            <a href="#" class="transition hover:text-blue-500">Mentions légales</a>
+            <a href="pages/nous_contacter.html" class="transition hover:text-blue-500">Contactez nous</a>
+            <a href="#" class="transition hover:text-blue-500">FAQ</a>
+        </div>
+    </div>
+</footer>
+
