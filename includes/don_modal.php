c49f9463391aedc3bfe9f02779b52260ce8435d2
ycngmn <41749571+ycngmn@users.noreply.github.com>
2025-11-08 00:10:22 +0100

    feat(don-modal): remplacer les boutons choix par un switch

--- includes/don_modal.php
+++ includes/don_modal.php
@@ -1,18 +1,24 @@
 <div id="fenetreDon" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden z-50 opacity-0 transition-opacity duration-300">
-    <div id="contenuModal"
-        class="bg-white rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2 relative p-6 transform scale-95 transition-transform duration-300">
-
+    <div id="contenuModal" class="bg-white rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2 relative p-6 transform scale-95 transition-transform duration-300">
         <button id="fermerModal"
             class="absolute top-4 right-4 text-gray-500 hover:text-gray-700 text-xl font-bold">&times;</button>
 
         <div class="flex flex-col md:flex-row items-center">
             <img src="../assets/img/don/donImage1.png" alt="illustration_egge_don" class="w-72 md:w-96 mb-4 md:mb-0 md:mr-6">
 
-            <article class="flex-1 flex flex-col items-center">
-                <div id="choixFrequence" class="flex justify-evenly items-center gap-4 p-4 rounded-lg bg-blue-200 mb-4 w-full">
-                    <button id="donUnique" class="px-4 py-2 bg-white rounded hover:bg-blue-300 transition">Je donne une fois</button>
-                    <div class="w-px h-6 bg-gray-300"></div>
-                    <button id="donMensuel" class="px-4 py-2 bg-white rounded hover:bg-blue-300 transition">Je donne tous les mois</button>
+            <article class="flex-1 flex flex-col items-center ">
+                <div class="flex items-center gap-x-4 py-8">
+                    <label for="toggle_don" class="text-base text-gray-700">Je donne une fois</label>
+                    <label for="toggle_don" class="relative inline-block w-14 h-8 cursor-pointer">
+                        <input type="checkbox" id="toggle_don" class="peer sr-only">
+                        <span class="absolute inset-0 bg-gray-300 rounded-full transition-colors duration-200 ease-in-out 
+                    peer-checked:bg-blue-600 peer-disabled:bg-gray-200 peer-disabled:opacity-60">
+                        </span>
+                        <span class="absolute top-1/2 left-1 -translate-y-1/2 size-6 bg-white rounded-full shadow-md 
+                    transition-transform duration-200 ease-in-out peer-checked:translate-x-6">
+                        </span>
+                    </label>
+                    <label for="toggle_don" class="text-base text-gray-700">Je donne tous les mois</label>
                 </div>
 
                 <section id="montantsDons" class="grid grid-cols-2 gap-4 w-full">
@@ -26,7 +32,7 @@
                     </div>
                 </section>
                 <button id="ouvrir-don" class="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 mt-4 rounded transform transition duration-300 hover:bg-blue-600 hover:scale-105 hover:shadow-lg">
-                    <img src="assets/img/accueil/heart.svg" alt="icon_don" class="w-6">
+                    <img src="../assets/img/accueil/heart.svg" alt="icon_don" class="w-6">
                     Je donne
                 </button>
             </article>
