93c1aaa606c55250c6fa9a2249ef98ebe198f0c0
ycngmn <41749571+ycngmn@users.noreply.github.com>
2025-11-07 10:05:53 +0100

    feat(footer): ajouter logo egee pour grands écrans

--- includes/footer.php
+++ includes/footer.php
@@ -1,23 +1,27 @@
-<footer class="flex flex-col md:flex-row w-full px-12 pt-20 pb-12 gap-8">
-    <div class="mr-1">
+<footer class="flex flex-col md:flex-row justify-between md:items-center w-full px-12 pt-20 pb-12 gap-8">
+    <div>
         <p class="text-blue-500 font-bold text-4xl">EGEE</p>
         <p>Entente des Générations pour l’Emploi et L’entreprise</p>
         <p>Association reconnu d’utilité publique</p>
 
         <div class="flex gap-4 items-center mt-8">
             <a href="https://www.instagram.com/association_egee/" class="transform transition duration-300 hover:scale-125">
-                <img src="/assets/img/instagram.svg" alt="">
+                <img src="/assets/img/instagram.svg" alt="logo-instagram">
             </a>
             <a href="https://fr.linkedin.com/company/egee-asso/" class="transform transition duration-300 hover:scale-125">
-                <img src="/assets/img/linkedin.svg" alt="">
+                <img src="/assets/img/linkedin.svg" alt="logo_linkedin">
             </a>
             <a href="https://www.facebook.com/egee.asso/" class="transform transition duration-300 hover:scale-125">
-                <img src="/assets/img/facebook.svg" alt="">
+                <img src="/assets/img/facebook.svg" alt="logo_facebook">
             </a>
         </div>
     </div>
 
-    <div id="info_section_footer" class="grid grid-cols-2 md:grid-cols-3 gap-8 md:ml-auto">
+    <a href="../index.php" class="hidden md:block">
+        <img src="../assets/img/logo_egee_footer.png" alt="egee_logo"
+            class="size-16 transform transition duration-300 hover:scale-105">
+    </a>
+    <div id="info_section_footer" class="grid grid-cols-2 md:grid-cols-3 gap-8 ">
         <div class="flex flex-col">
             <p class="font-bold text-xl mb-4 uppercase">Nos actions</p>
             <a href="/pages/Nos_Missions.php" class="transition hover:text-blue-500">Education</a>
@@ -42,4 +46,3 @@
         </div>
     </div>
 </footer>
-
